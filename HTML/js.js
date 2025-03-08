1e373d245ea007a305c17e54d977367c4be54d77
SANTHOSHKUMAR T <152668597+santhoshoggy@users.noreply.github.com>
2025-03-08 10:31:50 +0530

    Add files via upload

---
+++ HTML/js.js
@@ -0,0 +1,12 @@
+function myfunction(){
+document.getElementById("demo").innerText="hello alll..!!";
+document.getElementById("demo").style.color="blue";
+document.getElementById("demo2").style.color="yellow";
+
+
+}
+
+function myfunction1(){
+    document.getElementById("cat").innerText="yeah.! you clicked mee...";
+}
+
